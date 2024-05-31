fb0a9dfa754be7b4ff52ec6a368fd031559528c5
jiangfeng.11 <jiangfeng.11@bytedance.com>
2024-05-31 17:21:42 +0800

    feat: color scheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -2,10 +2,11 @@ return {
   {
     "rebelot/kanagawa.nvim",
   },
+  { "catppuccin/nvim", name = "catppuccin", priority = 1000, flavour = "mocha" },
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "kanagawa",
+      colorscheme = "catppuccin",
     },
   },
 }
